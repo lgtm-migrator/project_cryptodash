54873f9de3ce934aaf0d84f3c767e87c075e393b
Bhaskar Ram <bhaskarvilles@duck.com>
2022-10-07 11:49:21 +0530

    update

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -4,12 +4,10 @@ import { Row, Col } from 'antd';
 import { useGetExchangesQuery } from '../services/cryptoApi';
 import Loader from './Loader';
 
-/*const { Text } = Typography;
-const { Panel } = Collapse;*/
+
 
 const Exchanges = () => {
-  const { data, isFetching } = useGetExchangesQuery();
-  /*const exchangesList = data?.data?.exchanges;*/
+  const { isFetching } = useGetExchangesQuery();
   if (isFetching) return <Loader />;
 
   return (
